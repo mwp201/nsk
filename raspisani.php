eefa899183346e9e84549641134b2b66aef75197
Vjacheslav Kuznetsov <mwp201@yandex.ru>
2017-03-17 13:11:13 +0800

    create Db

--- raspisani.php
+++ raspisani.php
@@ -2,7 +2,3 @@
 $table = file_get_contents('http://raspisanie24.com/poezda/novosibirsk/');
 file_put_contents('rasp.html', $table);
 
-
-INSERT INTO `timetable`(`train_number`, `start_stantion`,
-`end_station`, `arrival_time`, `departure_time`, `parking_time`,
-`line_delete`) VALUES ('', '', '', ':', ':', '2 мин.', 'false')
